da600728570ec404158dfdb95773a636202342cb
Raphael Roberts <raphael.roberts48@gmail.com>
2019-04-06 18:35:55 -0500

    Set default font to roboto mom

--- init.el
+++ init.el
@@ -160,7 +160,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Roboto Mono" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
 ;; Monkey patch
 ;(defun quote-exe (path)
 ;  (if (string-match-p (regexp-quote " ") path)
