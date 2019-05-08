5e64b6f00cd12eeee8a3bfe2722d5a7521b74853
Raphael Roberts <raphael.roberts48@gmail.com>
2019-05-08 11:15:11 -0500

    Enabled upcase-region

--- init.el
+++ init.el
@@ -207,3 +207,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Roboto Mono" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
+(put 'upcase-region 'disabled nil)
