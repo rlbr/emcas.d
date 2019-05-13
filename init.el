15d16b9d7100ee8838a89e2eb6ab95aad7ba1e58
Raphael Roberts <raphael.roberts48@gmail.com>
2019-05-13 00:38:04 -0500

    Moved custom set variables

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 
 ;; INSTALL PACKAGES
 ;; --------------------------------------
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
 
 (require 'package)
 
@@ -165,21 +167,3 @@
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 (setq elpy-syntax-check-command python-check-command)
 ;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
- '(package-selected-packages
-   (quote
-    (lua-mode browse-kill-ring clipmon free-keys org-link-minor-mode js2-mode js2-refactor xref-js2 python-django django-mode visual-regexp-steroids pcre2el vimrc-mode iedit transient magit dracula-theme py-autopep8 flycheck elpy ein better-defaults ssh-config-mode yaml-mode apache-mode web-mode undo-tree))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Roboto Mono" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
-(put 'upcase-region 'disabled nil)
