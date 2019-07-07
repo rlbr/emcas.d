d1543caf463bb21d3f5662ced28ea3b081eddd6f
Raphael Roberts <raphael.roberts48@gmail.com>
2019-07-07 12:40:16 -0500

    Display line numbers is so pretty now

--- custom.el
+++ custom.el
@@ -15,5 +15,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Roboto Mono" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Roboto Mono" :foundry "outline" :slant normal :weight normal :height 113 :width normal))))
+ '(line-number-current-line ((t (:foreground "yellow" :weight bold)))))
 (put 'upcase-region 'disabled nil)
