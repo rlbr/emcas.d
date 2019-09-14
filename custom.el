8711828fbeb8ffb24eb8104003b04f62e75f08f2
Raphael Roberts <raphael.roberts48@gmail.com>
2019-09-14 02:36:45 -0500

    Fixed tramp for android and added lispy mode

--- custom.el
+++ custom.el
@@ -9,8 +9,9 @@
  '(elpy-eldoc-show-current-function nil)
  '(package-selected-packages
    (quote
-    (ibuffer-vc ag string-inflection tiny powershell diffview ivy-hydra window-numbering php-mode swiper mode-line-bell htmlize cython-mode blacken dired-narrow lua-mode browse-kill-ring clipmon free-keys org-link-minor-mode js2-mode js2-refactor xref-js2 python-django visual-regexp-steroids pcre2el vimrc-mode iedit transient magit dracula-theme flycheck elpy ein better-defaults ssh-config-mode yaml-mode apache-mode web-mode undo-tree)))
- '(reb-re-syntax (quote string)))
+    (kv lispy ibuffer-vc ag string-inflection tiny powershell diffview ivy-hydra window-numbering php-mode swiper mode-line-bell htmlize cython-mode blacken dired-narrow lua-mode browse-kill-ring clipmon free-keys org-link-minor-mode js2-mode js2-refactor xref-js2 python-django visual-regexp-steroids pcre2el vimrc-mode iedit transient magit dracula-theme flycheck elpy ein better-defaults ssh-config-mode yaml-mode apache-mode web-mode undo-tree)))
+ '(reb-re-syntax (quote string))
+ '(tramp-use-ssh-controlmaster-options nil nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
