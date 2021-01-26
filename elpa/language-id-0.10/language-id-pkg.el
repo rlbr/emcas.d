dab4dccc63e3fad9662d30a48e0b9e1b3193784a
Raphael Roberts <raphael.roberts48@gmail.com>
2021-01-25 23:01:23 -0600

    Updated packages and removed archives from being tracked

---
+++ elpa/language-id-0.10/language-id-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/raphael/.emacs.d/elpa/language-id-0.10/language-id.el  -*- no-byte-compile: t -*-
+(define-package "language-id" "0.10" "Library to work with programming language identifiers" '((emacs "24") (cl-lib "0.5")) :commit "3f0ad28202207c266bd8fc7904b224db69ceccf6" :authors '(("Lassi Kortela" . "lassi@lassi.io")) :maintainer '("Lassi Kortela" . "lassi@lassi.io") :keywords '("languages" "util") :url "https://github.com/lassik/emacs-language-id")
