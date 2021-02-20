f366ff7124f2d19032754a7b53b591daa4b4873e
Raphael Roberts <raphael.roberts48@gmail.com>
2021-02-20 02:00:37 -0600

    Update packages

---
+++ elpa/language-id-0.12/language-id-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/raphael/.emacs.d/elpa/language-id-0.12/language-id.el  -*- no-byte-compile: t -*-
+(define-package "language-id" "0.12" "Library to work with programming language identifiers" '((emacs "24") (cl-lib "0.5")) :commit "2c99ce29b86fc635649f4e89723912dc1cc4f36c" :authors '(("Lassi Kortela" . "lassi@lassi.io")) :maintainer '("Lassi Kortela" . "lassi@lassi.io") :keywords '("languages" "util") :url "https://github.com/lassik/emacs-language-id")
---
+++ elpa/ssh-config-mode-20210217.1051/ssh-config-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "ssh-config-mode" "20210217.1051" "Mode for fontification of ~/.ssh/config" 'nil :commit "820f60af17e71898303f4f3c2576f0619528a492" :authors
+  '(("Harley Gorrell" . "harley@panix.com"))
+  :maintainer
+  '("Harley Gorrell" . "harley@panix.com")
+  :keywords
+  '("ssh" "config" "emacs")
+  :url "https://github.com/jhgorrell/ssh-config-mode-el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
