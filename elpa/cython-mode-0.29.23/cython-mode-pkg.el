e11a7756140df87aa1c8742da0b7f5ed823a8fe2
Raphael Roberts <raphael.roberts48@gmail.com>
2021-05-22 02:45:26 -0500

    Updated packages

---
+++ elpa/cython-mode-0.29.23/cython-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from cython-mode.el  -*- no-byte-compile: t -*-
+(define-package "cython-mode" "0.29.23" "Major mode for editing Cython files" 'nil :commit "17670781083e3ccfedb1af4adcec614d4599eef9")
